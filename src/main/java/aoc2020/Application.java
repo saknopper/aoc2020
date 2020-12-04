332582f4c05e64ce0ca6ba9362cdce3d2d373373
Sander Knopper <sander@knopper-ict.nl>
2020-12-04 12:26:40 +0100

    Day 4

--- src/main/java/aoc2020/Application.java
+++ src/main/java/aoc2020/Application.java
@@ -7,41 +7,50 @@ import java.util.logging.Logger;
 import aoc2020.days.Day1;
 import aoc2020.days.Day2;
 import aoc2020.days.Day3;
+import aoc2020.days.Day4;
 
 public class Application
 {
-    private static final Logger log = Logger.getLogger(Application.class.getCanonicalName());
+	private static final Logger log = Logger.getLogger(Application.class.getCanonicalName());
 
-    public static void main(String[] args) throws URISyntaxException, IOException
-    {
-        log.info("Advent of Code 2020");
-        log.info("-----------------------------");
-        log.info("");
+	public static void main(String[] args) throws URISyntaxException, IOException
+	{
+		log.info("Advent of Code 2020");
+		log.info("-----------------------------");
+		log.info("");
 
-        Day1 day1 = new Day1();
-        int day1Part1Answer = day1.determineSumInExpenseReportAndGetProduct(2020, 2);
-        log.info("day 1 part 1, answer: " + day1Part1Answer);
+		Day1 day1 = new Day1();
+		int day1Part1Answer = day1.determineSumInExpenseReportAndGetProduct(2020, 2);
+		log.info("day 1 part 1, answer: " + day1Part1Answer);
 
-        int day1Part2Answer = day1.determineSumInExpenseReportAndGetProduct(2020, 3);
-        log.info("day 1 part 2, answer: " + day1Part2Answer);
+		int day1Part2Answer = day1.determineSumInExpenseReportAndGetProduct(2020, 3);
+		log.info("day 1 part 2, answer: " + day1Part2Answer);
 
-        Day2 day2 = new Day2();
-        int day2Part1Answer = day2.getAmountOfPasswordsAdheringToPolicy(false);
-        log.info("day 2 part 1, answer: " + day2Part1Answer);
+		Day2 day2 = new Day2();
+		int day2Part1Answer = day2.getAmountOfPasswordsAdheringToPolicy(false);
+		log.info("day 2 part 1, answer: " + day2Part1Answer);
 
-        int day2Part2Answer = day2.getAmountOfPasswordsAdheringToPolicy(true);
-        log.info("day 2 part 2, answer: " + day2Part2Answer);
+		int day2Part2Answer = day2.getAmountOfPasswordsAdheringToPolicy(true);
+		log.info("day 2 part 2, answer: " + day2Part2Answer);
 
-        Day3 day3 = new Day3();
-        long day3Part1Answer = day3.getAmountOfTreesEncounteredUsingSlope(3, 1);
-        log.info("day 3 part 1, answer: " + day3Part1Answer);
+		Day3 day3 = new Day3();
+		long day3Part1Answer = day3.getAmountOfTreesEncounteredUsingSlope(3, 1);
+		log.info("day 3 part 1, answer: " + day3Part1Answer);
 
-        long day3Part2Answer1 = day3.getAmountOfTreesEncounteredUsingSlope(1, 1);
-        long day3Part2Answer2 = day3.getAmountOfTreesEncounteredUsingSlope(3, 1);
-        long day3Part2Answer3 = day3.getAmountOfTreesEncounteredUsingSlope(5, 1);
-        long day3Part2Answer4 = day3.getAmountOfTreesEncounteredUsingSlope(7, 1);
-        long day3Part2Answer5 = day3.getAmountOfTreesEncounteredUsingSlope(1, 2);
-        long day3Part2Answer = day3Part2Answer1 * day3Part2Answer2 * day3Part2Answer3 * day3Part2Answer4 * day3Part2Answer5;
-        log.info("day 3 part 2, answer: " + day3Part2Answer);
-    }
+		long day3Part2Answer1 = day3.getAmountOfTreesEncounteredUsingSlope(1, 1);
+		long day3Part2Answer2 = day3.getAmountOfTreesEncounteredUsingSlope(3, 1);
+		long day3Part2Answer3 = day3.getAmountOfTreesEncounteredUsingSlope(5, 1);
+		long day3Part2Answer4 = day3.getAmountOfTreesEncounteredUsingSlope(7, 1);
+		long day3Part2Answer5 = day3.getAmountOfTreesEncounteredUsingSlope(1, 2);
+		long day3Part2Answer = day3Part2Answer1 * day3Part2Answer2 * day3Part2Answer3 * day3Part2Answer4
+				* day3Part2Answer5;
+		log.info("day 3 part 2, answer: " + day3Part2Answer);
+
+		Day4 day4 = new Day4();
+		long day4Part1Answer = day4.getAmountOfValidPassports(false);
+		log.info("day 4 part 1, answer: " + day4Part1Answer);
+
+		long day4Part2Answer = day4.getAmountOfValidPassports(true);
+		log.info("day 4 part 2, answer: " + day4Part2Answer);
+	}
 }
---
+++ src/main/java/aoc2020/days/Day4.java
@@ -0,0 +1,147 @@
+package aoc2020.days;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Day4
+{
+	private static final Logger log = Logger.getLogger(Day4.class.getCanonicalName());
+
+	private static final String[] REQUIRED_FIELDS = { "byr", "iyr", "eyr", "hgt", "hcl", "ecl",
+			"pid" /* , "cid" */ };
+	private static final String[] VALID_ECL_VALUES = { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+
+	public long getAmountOfValidPassports(boolean checkData) throws URISyntaxException, IOException
+	{
+		final List<Map<String, String>> passports = new ArrayList<>();
+
+		Path path = Paths.get(getClass().getClassLoader().getResource("day4.txt").toURI());
+
+		Map<String, String> currentPassword = new HashMap<>();
+		Stream<String> lines = Files.lines(path);
+		List<String> linesList = lines.collect(Collectors.toList());
+		// Add empty line at the end to make sure the last passport is added to
+		// the list
+		linesList.add("");
+		lines.close();
+
+		for (String l : linesList) {
+			if (l.isEmpty()) {
+				passports.add(currentPassword);
+				currentPassword = new HashMap<>();
+				continue;
+			}
+
+			String[] fieldPairs = l.split(" ");
+			for (String pair : fieldPairs) {
+				String[] keyAndValue = pair.split(":");
+				currentPassword.put(keyAndValue[0], keyAndValue[1]);
+			}
+		}
+
+		if (checkData) {
+			return passports.stream().filter(Day4::containsRequiredFieldsAndValidData).count();
+		} else {
+			return passports.stream().filter(Day4::containsRequiredFields).count();
+		}
+	}
+
+	private static boolean containsRequiredFields(Map<String, String> passport)
+	{
+		for (String field : REQUIRED_FIELDS) {
+			if (!passport.containsKey(field)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private static boolean containsRequiredFieldsAndValidData(Map<String, String> passport)
+	{
+		for (String field : REQUIRED_FIELDS) {
+			if (!passport.containsKey(field)) {
+				return false;
+			}
+		}
+
+		String byr = passport.get("byr");
+		if (!validNumber(byr, 4, 1920, 2002)) {
+			return false;
+		}
+
+		String iyr = passport.get("iyr");
+		if (!validNumber(iyr, 4, 2010, 2020)) {
+			return false;
+		}
+
+		String eyr = passport.get("eyr");
+		if (!validNumber(eyr, 4, 2020, 2030)) {
+			return false;
+		}
+
+		String hgt = passport.get("hgt");
+		try {
+			int hgtInt = Integer.parseInt(hgt.substring(0, hgt.length() - 2));
+			if (hgt.endsWith("cm")) {
+				if (hgtInt < 150 || hgtInt > 193) {
+					return false;
+				}
+			} else if (hgt.endsWith("in")) {
+				if (hgtInt < 59 || hgtInt > 76) {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		} catch (NumberFormatException e) {
+			return false;
+		}
+
+		String hcl = passport.get("hcl");
+		if (!hcl.startsWith("#") || hcl.length() != 7) {
+			return false;
+		}
+		try {
+			Integer.parseInt(hcl.substring(1), 16);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+
+		String ecl = passport.get("ecl");
+		if (!List.of(VALID_ECL_VALUES).contains(ecl)) {
+			return false;
+		}
+
+		String pid = passport.get("pid");
+		if (!pid.matches("^(\\d){9}$")) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private static boolean validNumber(String number, int digits, int min, int max)
+	{
+		if (number.length() > digits) {
+			return false;
+		}
+
+		int numberAsInt = Integer.parseInt(number);
+		if (numberAsInt < min || numberAsInt > max) {
+			return false;
+		}
+
+		return true;
+	}
+}
