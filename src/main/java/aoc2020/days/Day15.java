193197fd419080c0b8a2a3645d7927a8bda6fa3b
Sander Knopper <s.knopper@flusso.nl>
2020-12-15 12:18:35 +0100

    Day 15 - optimize

--- src/main/java/aoc2020/days/Day15.java
+++ src/main/java/aoc2020/days/Day15.java
@@ -6,9 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Day15
 {
@@ -31,80 +29,28 @@ public class Day15
 
     public long getNumberAtRound(int maxRounds)
     {
-        final Map<Long, NumberInfo> numbersAndTurns = new HashMap<>();
+        int[] lastSeenInTurn = new int[maxRounds];
 
-        long lastNumber = 0L;
-        int curRound = 0;
+        int curRound = 1;
         for (var n : startNumbers) {
-            NumberInfo numberInfo = new NumberInfo();
-            numberInfo.pushPreviousTurn(curRound);
-            numbersAndTurns.put(n.longValue(), numberInfo);
-
-            lastNumber = n;
+            lastSeenInTurn[n] = curRound;
             curRound++;
         }
 
-        for (; curRound < maxRounds; curRound++) {
-            lastNumber = getAndStoreNumberForRound(curRound, lastNumber, numbersAndTurns);
+        int lastNumber = startNumbers.get(startNumbers.size() - 1);
+
+        for (; curRound <= maxRounds; curRound++) {
+            int newNumber;
+            if (lastSeenInTurn[lastNumber] != 0) {
+                newNumber = (curRound - 1) - lastSeenInTurn[lastNumber];
+            } else {
+                newNumber = 0;
+            }
+
+            lastSeenInTurn[lastNumber] = curRound - 1;
+            lastNumber = newNumber;
         }
 
         return lastNumber;
-    }
-
-    private static long getAndStoreNumberForRound(int round, long lastNumber, Map<Long, NumberInfo> numbersAndTurns)
-    {
-        long newNumber;
-
-        NumberInfo nInfo = numbersAndTurns.get(lastNumber);
-        if (nInfo.getPreviousPreviousTurn() != -1) {
-            newNumber = nInfo.getPreviousTurn() - (long) nInfo.getPreviousPreviousTurn();
-        } else {
-            newNumber = 0L;
-        }
-
-        if (numbersAndTurns.containsKey(newNumber)) {
-            numbersAndTurns.get(newNumber).pushPreviousTurn(round);
-        } else {
-            NumberInfo numberInfo = new NumberInfo();
-            numberInfo.pushPreviousTurn(round);
-            numbersAndTurns.put(newNumber, numberInfo);
-        }
-
-        return newNumber;
-    }
-
-    private static class NumberInfo
-    {
-        private int previousTurn;
-        private int previousPreviousTurn;
-
-        public NumberInfo()
-        {
-            super();
-            previousTurn = -1;
-            previousPreviousTurn = -1;
-        }
-
-        public int getPreviousTurn()
-        {
-            return previousTurn;
-        }
-
-        public int getPreviousPreviousTurn()
-        {
-            return previousPreviousTurn;
-        }
-
-        public void pushPreviousTurn(int previousTurn)
-        {
-            this.previousPreviousTurn = this.previousTurn;
-            this.previousTurn = previousTurn;
-        }
-
-        @Override
-        public String toString()
-        {
-            return "NumberInfo [previousTurn=" + previousTurn + ", previousPreviousTurn=" + previousPreviousTurn + "]";
-        }
     }
 }
