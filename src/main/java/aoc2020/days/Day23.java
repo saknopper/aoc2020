0b7e5a8c883509d4535d7b44b09c397d536d15f7
Sander Knopper <s.knopper@flusso.nl>
2020-12-24 09:35:06 +0100

    Day 23 - final cleanups

--- src/main/java/aoc2020/days/Day23.java
+++ src/main/java/aoc2020/days/Day23.java
@@ -22,11 +22,10 @@ public class Day23
         Integer min = 1;
         Integer max = 9;
 
-        final Map<Integer, Integer> nextCupCache = new HashMap<>();
-
         final List<Integer> cups = new ArrayList<>();
         INPUT.chars().forEach(label -> cups.add(Integer.valueOf(String.valueOf((char) label))));
 
+        final Map<Integer, Integer> nextCupCache = new HashMap<>(cups.size());
         for (int i = 0; i < cups.size() - 1; i++) {
             nextCupCache.put(cups.get(i), cups.get(i + 1));
         }
@@ -41,8 +40,7 @@ public class Day23
         Integer current = nextCupCache.get(1);
         for (int i = 1; i < max; i++) {
             sb.append(current);
-            Integer next = nextCupCache.get(current);
-            current = next;
+            current = nextCupCache.get(current);
         }
 
         return sb.toString();
@@ -54,14 +52,13 @@ public class Day23
         final Integer max = 1000000;
         final int rounds = 10000000;
 
-        final Map<Integer, Integer> nextCupCache = new HashMap<>();
-
         final List<Integer> cups = new ArrayList<>(max);
         INPUT.chars().forEach(label -> cups.add(Integer.valueOf(String.valueOf((char) label))));
         for (int i = 10; i <= max; i++) {
             cups.add(Integer.valueOf(i));
         }
 
+        final Map<Integer, Integer> nextCupCache = new HashMap<>(cups.size());
         for (int i = 0; i < cups.size() - 1; i++) {
             nextCupCache.put(cups.get(i), cups.get(i + 1));
         }
@@ -81,13 +78,11 @@ public class Day23
     private Integer playRound(final Map<Integer, Integer> cache, final Integer currentCup, final Integer min,
             final Integer max)
     {
-        final List<Integer> subCups = new ArrayList<>();
         Integer c1 = cache.get(currentCup);
         Integer c2 = cache.get(c1);
         Integer c3 = cache.get(c2);
-        subCups.add(c1);
-        subCups.add(c2);
-        subCups.add(c3);
+
+        final List<Integer> subCups = List.of(c1, c2, c3);
 
         Integer destination = getDestinationCup(currentCup, subCups, min, max);
 
@@ -101,10 +96,10 @@ public class Day23
     private Integer getDestinationCup(final Integer currentCup, final List<Integer> subCups, final Integer min,
             final Integer max)
     {
-        Integer currentMinusOne = Integer.valueOf(currentCup.intValue() - 1);
+        Integer currentMinusOne = currentCup - 1;
         Integer valueToFind = currentMinusOne.equals(ZERO) ? max : currentMinusOne;
         while (subCups.contains(valueToFind)) {
-            valueToFind = Integer.valueOf(valueToFind.intValue() - 1);
+            valueToFind = valueToFind - 1;
             if (valueToFind.compareTo(min) < 0) {
                 valueToFind = max;
             }
